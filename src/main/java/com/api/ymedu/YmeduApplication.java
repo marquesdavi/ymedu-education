cdb959ee0fe89babed60f103e932e2c42695eb67
Daví Marques <50294449+marquesdavi@users.noreply.github.com>
2024-03-12 18:54:28 -0300

    project setup

---
+++ src/main/java/com/api/ymedu/YmeduApplication.java
@@ -0,0 +1,13 @@
+package com.api.ymedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmeduApplication.class, args);
+	}
+
+}
