9702ad584dfdb7f65286f82e621ec8511ce7ed0f
Matheus Gomes <matheus.maciel@boxxinsurance.com>
2024-03-13 14:23:04 -0300

    Criado entidade de curso

---
+++ src/main/java/com/api/ymedu/persistence/entities/CourseEntity.java
@@ -0,0 +1,27 @@
+package com.api.ymedu.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Courses", schema = "public")
+public class CourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private String instructor;
+    @Column(nullable = false)
+    private String duration;
+}
